8c341fd77a82ca5ebb7bd5299d3e09ee54997968
Tsi2216 <getanehshimelis40@gmail.com>
2024-12-18 10:36:43 -0800

    added SearchBar.jsx and WeatherCard.jsx

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  esbuild: {
-    loader: {
-      '.js': 'jsx', // Set the loader for .js files to jsx
+  resolve: {
+    alias: {
+      '@': '/src', 
     },
+    extensions: ['.js', '.jsx'], 
   },
 });
